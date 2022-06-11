a78cc879b74d168bd13a46a76f5ef18d4853968c
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-06-11 19:18:19 +0200

    moved dummy_ Data array and .map to forecastList component to leave a pp.js leaner

--- src/App.js
+++ src/App.js
@@ -2,25 +2,10 @@ import "./App.css";
 import ForecastList from "./components/ForecastList";
 
 function App() {
-  const DUMMY_DATA = [
-    {
-      city: "London",
-      temperature: "25 degrees",
-    },
-    { city: "Roma", temperature: "30 degrees" },
-  ];
-
   return (
     <>
       <h1> Weather App </h1>
       <ForecastList />
-      {DUMMY_DATA.map((item) => (
-        <ul>
-          <li>
-            {item.city} {item.temperature}
-          </li>
-        </ul>
-      ))}
     </>
   );
 }
--- src/components/ForecastList.js
+++ src/components/ForecastList.js
@@ -1,9 +1,22 @@
 import React from "react";
 
 const ForecastList = () => {
+  const DUMMY_DATA = [
+    { city: "London", weather: " sunny", temperature: "25 degrees" },
+    { city: "Roma", weather: " cloudy", temperature: "30, degrees" },
+    { city: "New York", weather: " rainy", temperature: "24 degrees" },
+    { city: "Oslo", weather: " cloudy", temperature: "18 degrees" },
+  ];
   return (
     <div>
-      <p>My forecast for the day:</p>
+      <p>Next weather forecast of the day:</p>
+      {DUMMY_DATA.map((item, index) => (
+        <ul key={index}>
+          <li>
+            {item.city}, {item.weather}, {item.temperature}
+          </li>
+        </ul>
+      ))}
     </div>
   );
 };

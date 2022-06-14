621937f149fd5bf532f214dfc8b85e1c2ccda1bb
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-06-14 11:42:49 +0200

    working on rendering forecast list from api with map in jsx

--- src/components/ForecastList.js
+++ src/components/ForecastList.js
@@ -1,12 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ForecastList = () => {
-  // const DUMMY_DATA = [
-  //   { city: "London", weather: " sunny", temperature: "25 degrees" },
-  //   { city: "Roma", weather: " cloudy", temperature: "30, degrees" },
-  //   { city: "New York", weather: " rainy", temperature: "24 degrees" },
-  //   { city: "Oslo", weather: " cloudy", temperature: "18 degrees" },
-  // ];
+  const [myList, setList] = useState([]);
 
   fetch(
     "https://api.openweathermap.org/data/2.5/forecast?lat=41.93012&lon=2.25486&appid=24c3564ab328937258934fca6c93f832"
@@ -14,39 +9,37 @@ const ForecastList = () => {
     .then((response) => {
       return response.json();
     })
-    .then((data) => {
-      console.log(data.city.name);
 
-      return data.city.name;
+    .then((data) => {
+      const array = data.list;
+      console.log(array, "array");
+      setList(array);
     });
 
-  // });
-  // data.list.map((item, index) => {
   return (
     <div>
       <p> Weather forecast:</p>
+
       <ul>
-        <li>{}</li>
+        <li>{myList.main}</li>
       </ul>
     </div>
   );
-  // });
 };
 
 export default ForecastList;
 
+// const DUMMY_DATA = [
+//   { city: "London", weather: " sunny", temperature: "25 degrees" },
+//   { city: "Roma", weather: " cloudy", temperature: "30, degrees" },
+//   { city: "New York", weather: " rainy", temperature: "24 degrees" },
+//   { city: "Oslo", weather: " cloudy", temperature: "18 degrees" },
+// ];
+
+// data.list.map((item, index) => {
 //  {/* <ul key={index}> */}
 //  <li>
 //  {item.data.city.name}, {item.data},
 //  {item.temperature}
 // </li>
 // </ul>
-{
-  /* {Dummy_data.map((item, index) => (
-        <ul key={index}>
-          <li>
-            {item.city}, {item.weather}, {item.temperature}
-          </li>
-        </ul>
-      ))} */
-}

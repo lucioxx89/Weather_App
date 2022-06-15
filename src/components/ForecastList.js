7a424b43809f9ee71b2db0335ae42850d2613ae3
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-06-15 12:21:19 +0200

    addd useEffect to prevent loop on mounting, mapped the temperature and rendered them in ul

--- src/components/ForecastList.js
+++ src/components/ForecastList.js
@@ -1,33 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ForecastList = () => {
   const [forecastList, setForecastList] = useState([]);
   const [city, setCity] = useState("");
 
-  fetch("http://localhost:3000/forecast.json")
-    // fetch(
-    //   "https://api.openweathermap.org/data/2.5/forecast?lat=41.93012&lon=2.25486&appid=24c3564ab328937258934fca6c93f832"
-    // )
-    .then((response) => {
-      return response.json();
-    })
+  useEffect(() => {
+    fetch("http://localhost:3000/forecast.json")
+      // fetch(
+      //   "https://api.openweathermap.org/data/2.5/forecast?lat=41.93012&lon=2.25486&appid=24c3564ab328937258934fca6c93f832"
+      // )
+      .then((response) => {
+        return response.json();
+      })
 
-    .then((data) => {
-      // const forecastArray = data.list.map((item) => {
-      //   return item.main.temp;
-      // });
-      // setForecastList(forecastArray);
-      setCity(data.city.name);
+      .then((data) => {
+        // const forecastArray = data.list[0].main.temp.map((item) => {
+        //   return item;
+        // });
+        // setForecastList(forecastArray);
 
-      console.log(data.city.name, "List from API");
-    });
+        setCity(data.city.name);
+        setForecastList(data.list);
+
+        console.log(data.city.name, "List from API");
+        console.log(data.list, "List from API");
+      });
+  }, []);
 
   return (
     <div>
-      <p> Weather forecast for {city}:</p>
+      <h3> Weather forecast for {city} :</h3>
+      {forecastList.map((item, index) => {
+        return (
+          <ul key={index}>
+            <li> Temperature : {item.main.temp} </li>
+          </ul>
+        );
+      })}
 
       <ul>
-        <li>{forecastList} </li>
+        <li>{} </li>
       </ul>
     </div>
   );

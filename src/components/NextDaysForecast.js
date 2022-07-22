ae99bca2fe76bd1031086cf129d3e3a919b25fdb
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-07-22 18:37:36 +0200

    added new api url, show temperatures and description but problem with cors, didnt solve it

--- src/components/NextDaysForecast.js
+++ src/components/NextDaysForecast.js
@@ -17,7 +17,7 @@ const NextDaysForecast = (props) => {
   useEffect(() => {
     fetch(
       // `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=metric`
-      // "http://localhost:3000/forecast.json"
+      // "http://localhost:3000/daily.json"
 
       `https://api.weatherbit.io/v2.0/forecast/daily?city=${location}&key=${API_KEY}`
     )
@@ -30,7 +30,7 @@ const NextDaysForecast = (props) => {
       .then((data) => {
         console.log(data, "Next days forecast");
 
-        setHoursForecast(data.list);
+        setHoursForecast(data.data);
       })
       .catch((error) => {
         console.log(error.message);
@@ -39,6 +39,36 @@ const NextDaysForecast = (props) => {
 
   return (
     <div>
+      {hoursForecast.map((item, index) => {
+        const date = item.valid_date;
+        const MinForecastTemperature = item.low_temp;
+        const MaxForecastTemperature = item.max_temp;
+        const forecastDescription = item.weather.description;
+
+        return (
+          <div key={index} className="bottom_cards">
+            <div>
+              <div className="forecast_date">Date: {date}</div>
+
+              <div className="forecast_temp">
+                {" "}
+                Min Temperature: {MinForecastTemperature}℃
+              </div>
+
+              <div className="forecast_temp">
+                {" "}
+                Max Temperature: {MaxForecastTemperature}℃
+              </div>
+
+              <div className="forecast_description">
+                {" "}
+                Description: {forecastDescription}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+
       {/* {hoursForecast.map((item, index) => {
         const time = item.dt_txt;
         const forecastTemperature = (item.main.temp / 1).toFixed(1);

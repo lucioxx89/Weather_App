b114992afbf9895d79b0ad78e8b496fc65924fa4
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-07-28 21:25:47 +0200

    refactor weatherforecastComponent

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -11,16 +11,7 @@ const SearchForm = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState("");
   const [location, setLocation] = useState("");
-  const [city, setCity] = useState("");
-  const [country, setCountry] = useState("");
-  const [temperature, setTemperature] = useState("");
-  const [humidity, setHumidity] = useState("");
-  const [weatherDescription, setWeatherDescription] = useState("");
-  const [tempMin, setTempMin] = useState("");
-  const [tempMax, setTempMax] = useState("");
-  const [wind, setWind] = useState("");
-  const [feelsLike, setFeelsLike] = useState("");
-  const [icon, setIcon] = useState("");
+  const [currentWeatherData, setCurrentWeatherData] = useState({});
 
   const onSubmitHandler = (event) => {
     setLoading("Loading...");
@@ -40,32 +31,13 @@ const SearchForm = () => {
 
         setError("");
         setLoading("");
-
-        setCity(data);
-        setCity(data.name);
-        setCountry(data.sys);
-        setTemperature(data.main.temp);
-        setTempMin(data.main.temp_min);
-        setTempMax(data.main.temp_max);
-        setFeelsLike(data.main.feels_like.toFixed(1));
-        setHumidity(data.main.humidity);
-        setWeatherDescription(data.weather[0].description);
-
-        setLocation();
-        setWind(data.wind.speed.toFixed());
+        setLocation("");
+        setCurrentWeatherData(data);
       })
       .catch((error) => {
         setLoading("");
         setError(error.message);
-        setCity("");
-        setCountry("");
-        setTemperature("");
-        setTempMin("");
-        setTempMax("");
-        setFeelsLike("");
-        setHumidity("");
-        setWeatherDescription("");
-        setWind("");
+        setCurrentWeatherData({});
         console.log(error.message);
       });
     setLocation(""); //set it back to empty string so input is empty again
@@ -75,13 +47,14 @@ const SearchForm = () => {
     setLocation(event.target.value);
   };
 
-  //copied
-  const Temperature_Rounded = Number(temperature / 1).toFixed(1);
-  const FeelsLike_Rounded = Number(feelsLike / 1).toFixed(1);
-  const Min_Temperature_Rounded = Number(tempMin / 1).toFixed(1);
-  const Max_Temperature_Rounded = Number(tempMax / 1).toFixed(1);
-  const weatherDescriptionCapLetter =
-    weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
+  let currentWeatherComponent;
+  if (currentWeatherData.name) {
+    currentWeatherComponent = (
+      <WeatherForecast currentWeatherData={currentWeatherData} />
+    );
+  } else {
+    currentWeatherComponent = "";
+  }
 
   return (
     <div className="main_body">
@@ -104,7 +77,9 @@ const SearchForm = () => {
       <p className="loading">{loading}</p>
       <p className="error"> {error} </p>
 
-      <WeatherForecast
+      {currentWeatherComponent}
+
+      {/* <WeatherForecast
         city={city}
         country={country}
         Temperature_Rounded={Temperature_Rounded}
@@ -114,9 +89,9 @@ const SearchForm = () => {
         Min_Temperature_Rounded={Min_Temperature_Rounded}
         Max_Temperature_Rounded={Max_Temperature_Rounded}
         wind={wind}
-      />
+      /> */}
 
-      <NextDaysForecast location={location} />
+      {/* <NextDaysForecast location={location} /> */}
     </div>
   );
 };
--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -2,49 +2,68 @@ import React from "react";
 
 import "./WeatherForecast.css";
 
-const WeatherForecast = (props) => {
-  return (
-    <div className="weather_container">
-      <div className="top_info">
-        {" "}
-        <div className="location">
-          {props.city}{" "}
-          <span className="country">({props.country.country})</span>
-        </div>
-        <div className="actual_temperature">{props.Temperature_Rounded}℃</div>
-        <div className="weather_description">
-          {" "}
-          {props.weatherDescriptionCapLetter}{" "}
-        </div>
-      </div>
+const WeatherForecast = ({ currentWeatherData }) => {
+  // let { currentWeatherData } = props;
 
-      <div className="bottom_info">
-        <div className="humidity">
-          <p>{props.humidity}%</p>
-          <p>Humidity</p>{" "}
-        </div>
-        <div className="feels_like">
-          <p> {props.FeelsLike_Rounded}℃</p>
-          <p> Feels like </p>
-        </div>
+  // / const Temperature_Rounded = Number(temperature / 1).toFixed(1);
+  // // const FeelsLike_Rounded = Number(feelsLike / 1).toFixed(1);
+  // // const Min_Temperature_Rounded = Number(tempMin / 1).toFixed(1);
+  // // const Max_Temperature_Rounded = Number(tempMax / 1).toFixed(1);
+  // // const weatherDescriptionCapLetter =
+  // //   weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
 
-        <div className="temp_min">
-          <p>{props.Min_Temperature_Rounded}℃</p>
-          <p>Min</p>
-        </div>
+  return <div>{currentWeatherData.name}</div>;
 
-        <div className="temp_max">
-          <p>{props.Max_Temperature_Rounded}℃</p>
-          <p>Max</p>
-        </div>
+  // For reference
+  // setCity(data);
+  // setCity(data.name);
+  // setCountry(data.sys);
+  // setTemperature(data.main.temp);
+  // setTempMin(data.main.temp_min);
+  // setTempMax(data.main.temp_max);
+  // setFeelsLike(data.main.feels_like.toFixed(1));
+  // setHumidity(data.main.humidity);
+  // setWeatherDescription(data.weather[0].description);
+  // setWind(data.wind.speed.toFixed());
 
-        <div className="wind_speed">
-          <p>{props.wind}MPS</p>
-          <p>Wind Speed</p>
-        </div>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className="weather_container">
+  //     <div className="top_info">
+  //       {" "}
+  //       <div className="location">
+  //         {props.city}{" "}
+  //         <span className="country">({props.country.country})</span>
+  //       </div>
+  //       <div className="actual_temperature">{props.Temperature_Rounded}℃</div>
+  //       <div className="weather_description">
+  //         {" "}
+  //         {props.weatherDescriptionCapLetter}{" "}
+  //       </div>
+  //     </div>
+  //     <div className="bottom_info">
+  //       <div className="humidity">
+  //         <p>{props.humidity}%</p>
+  //         <p>Humidity</p>{" "}
+  //       </div>
+  //       <div className="feels_like">
+  //         <p> {props.FeelsLike_Rounded}℃</p>
+  //         <p> Feels like </p>
+  //       </div>
+  //       <div className="temp_min">
+  //         <p>{props.Min_Temperature_Rounded}℃</p>
+  //         <p>Min</p>
+  //       </div>
+  //       <div className="temp_max">
+  //         <p>{props.Max_Temperature_Rounded}℃</p>
+  //         <p>Max</p>
+  //       </div>
+  //       <div className="wind_speed">
+  //         <p>{props.wind}MPS</p>
+  //         <p>Wind Speed</p>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default WeatherForecast;
